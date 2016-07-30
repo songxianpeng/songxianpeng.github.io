6340f495ccff9a7f23aa100b30e35406ea83277c
xpressSong <707339962@qq.com>
2016-07-31 00:10:27 +0800

    toc

--- assets/js/jquery.toc.js
+++ assets/js/jquery.toc.js
@@ -8,7 +8,7 @@
       minimumHeaders: 2,
       headers: 'h1, h2, h3, h4, h5, h6',
       listType: 'ul', // values: [ol|ul]
-      showEffect: 'show', // values: [show|slideDown|fadeIn|none]
+      showEffect: 'slideDown', // values: [show|slideDown|fadeIn|none]
       showSpeed: 0 // set to 0 to deactivate effect
     },
     settings = $.extend(defaults, options);
